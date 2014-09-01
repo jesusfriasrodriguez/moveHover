5989283bbc9f7b5e06cd965590505c0297270967
injectilo <noquedanadalibre@gmail.com>
2014-09-01 12:00:16 +0200

    upload moveHover files

---
+++ js/movehover.js
@@ -0,0 +1,37 @@
+(function($) {
+	$.fn.extend({
+		movehover: function(id) {
+			//debugger;
+			return this.each(function(){
+
+				var imageWidth = $('#' + this.id + " img").width();
+				var imageHeight = $('#' + this.id + " img").height();
+
+				$.fn.middle = (function() {
+					$(this).scrollLeft((imageWidth / 2) - ($(this).width() / 2));
+					$(this).scrollTop((imageHeight / 2) - ($(this).height() / 2));
+				});
+
+				$(this).middle();
+
+				$(this).mousemove(function(event){
+				
+		 			var offset = $(this).offset();
+		            positionX = event.pageX - offset.left;
+		            positionY = event.pageY - offset.top;
+					
+				   	var horizontalMousePosition = event.pageX;
+				   	var verticalMousePosition = event.pageY;
+
+			   	  	$(this).scrollTop(positionY * (imageHeight / $(this).height()));
+			   	  	$(this).scrollLeft(positionX * (imageWidth / $(this).width()));
+				});
+				
+				$(this).mouseout(function() {
+					$(this).scrollLeft((imageWidth / 2) - ($(this).width() / 2));
+					$(this).scrollTop((imageHeight / 2) - ($(this).height() / 2));
+				});
+			});
+		}
+	});
+})(jQuery)
