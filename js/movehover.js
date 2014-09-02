4c00c06a0cef053c45dfbd73b1508fb68ad784e4
injectilo <noquedanadalibre@gmail.com>
2014-09-02 17:56:30 +0200

    Adjust move

--- js/movehover.js
+++ js/movehover.js
@@ -8,14 +8,15 @@
 				var imageHeight = $('#' + this.id + " img").height();
 
 				$.fn.middle = (function() {
+					// center
 					$(this).scrollLeft((imageWidth / 2) - ($(this).width() / 2));
 					$(this).scrollTop((imageHeight / 2) - ($(this).height() / 2));
 				});
-
+				
 				$(this).middle();
 
 				$(this).mousemove(function(event){
-				
+
 		 			var offset = $(this).offset();
 		            positionX = event.pageX - offset.left;
 		            positionY = event.pageY - offset.top;
@@ -23,13 +24,9 @@
 				   	var horizontalMousePosition = event.pageX;
 				   	var verticalMousePosition = event.pageY;
 
-			   	  	$(this).scrollTop(positionY * (imageHeight / $(this).height()));
-			   	  	$(this).scrollLeft(positionX * (imageWidth / $(this).width()));
-				});
-				
-				$(this).mouseout(function() {
-					$(this).scrollLeft((imageWidth / 2) - ($(this).width() / 2));
-					$(this).scrollTop((imageHeight / 2) - ($(this).height() / 2));
+			   	  	$(this).scrollTop( positionY * (imageHeight / $(this).height()) - positionY);
+			   	  	$(this).scrollLeft( positionX * (imageWidth / $(this).width()) - positionX);
+
 				});
 			});
 		}
